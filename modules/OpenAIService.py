3792a2758694b84edb89a2225c88f9112d82bb73
Michal Jankowski <jankowski.m@gmail.com>
2023-03-20 23:06:05 +0100

    Azure service added

--- modules/OpenAIService.py
+++ modules/OpenAIService.py
@@ -3,8 +3,19 @@ import openai
 
 class OpenAIService:
     def init_connection(self):
-       openai.organization = st.secrets["organisation"]
-       openai.api_key =  st.secrets["apiKey"]
+        self.init_openAIConnetion()
+
+    def init_openAIConnetion(self):
+        st.warning("Working wih OpenAI service")
+        openai.organization = st.secrets["organisation"]
+        openai.api_key =  st.secrets["apiKey"]
+
+    def init_AzureConnection(self):
+        st.warning("Working wih Azure service")
+        openai.api_type = "azure"
+        openai.api_key = st.secrets["azureKey"]
+        openai.api_base = st.secrets["azureEndPoint"]
+        openai.api_version = "2022-12-01"
 
     def modelsList(self):
         return openai.Model.list()
